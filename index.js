0f43306943e51556e23ba05b9061122ee137eaf5
hasan-naim <hasannaim6246@gmail.com>
2022-10-26 16:14:12 +0600

    course api now works

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const catagory = require("./data/catagory.json");
+const courses = require("./data/card.json");
 
 const app = express();
 const port = 5000;
@@ -14,6 +15,9 @@ app.get("/", (req, res) => {
 
 app.get("/catagories", (req, res) => {
   res.json(catagory);
+});
+app.get("/courses", (req, res) => {
+  res.json(courses);
 });
 
 app.listen(port, () => {
