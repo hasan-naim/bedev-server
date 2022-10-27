13b23cdc08188850a2bcb3e05e2aa9296784272c
hasan-naim <hasannaim6246@gmail.com>
2022-10-27 11:06:42 +0600

    details api is now works

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const catagory = require("./data/catagory.json");
 const courses = require("./data/card.json");
+const details = require("./data/details.json");
 
 const app = express();
 const port = 5000;
@@ -18,6 +19,13 @@ app.get("/catagories", (req, res) => {
 });
 app.get("/courses", (req, res) => {
   res.json(courses);
+});
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  console.log(details);
+  const respon = details.find((obj) => obj.id == id);
+  res.json(respon);
 });
 
 app.listen(port, () => {
